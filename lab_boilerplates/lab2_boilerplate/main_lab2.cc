45166575a50565682f2f1d795468b12c08da451c
Logan Evans <loganpevans@gmail.com>
2013-01-08 15:34:37 -0800

    Initial commit.

---
+++ lab_boilerplates/lab2_boilerplate/main_lab2.cc
@@ -0,0 +1,7 @@
+#include "./definitions_lab2.h"
+
+int main(int argc, char** argv) {
+  printf("This is printed in main_lab2.cc\n");
+  foobar(argc, argv);
+}
+
---
+++ lab_boilerplates/lab2_boilerplate/utilities_lab2.cc
@@ -0,0 +1,6 @@
+#include "./definitions_lab2.h"
+
+void foobar(int argc, char** argv) {
+  printf("This is printed in foobar().\n");
+}
+
